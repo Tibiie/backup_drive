e7714a178091d8d5f57b1aa6e9a30ea1a7216ec3
Tibiie <yuraniecheverri8@gmail.com>
2025-02-17 12:33:01 -0500

    aa

--- backup.py
+++ backup.py
@@ -1,12 +1,13 @@
 import os
 import zipfile
+from conexion_database import obtener_archivos_desde_bd
 from conexion_drive import obtener_archivos_en_drive
 
 ZIP_FILE_PATH = "backup.zip"
 DRIVE_FOLDER_ID = '1qNiS0eHrAp-9b5SWqexh0HErxAEL5Gmq'  
 
 def comprimir_archivos(archivos, zip_path):
-    """Comprime los archivos en un ZIP y los sube a Google Drive."""
+    """Comprime los archivos en un ZIP."""
     with zipfile.ZipFile(zip_path, 'w') as zipf:
         for archivo in archivos:
             if os.path.exists(archivo):
@@ -27,6 +28,7 @@ def agregar_archivos_nuevos_a_zip(archivos, zip_path):
     if archivos_nuevos:
         print(f"Archivos nuevos a agregar: {archivos_nuevos}")
         comprimir_archivos([archivos_locales[file] for file in archivos_nuevos], zip_path)
+        return True 
     else:
         print("No hay archivos nuevos para agregar.")
-
+        return False  
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from backup import agregar_archivos_nuevos_a_zip
 from upload import subir_archivo_a_drive
 from conexion_database import obtener_archivos_desde_bd
-from backup import agregar_archivos_nuevos_a_zip 
 
 def main():
     archivos = obtener_archivos_desde_bd()
     if archivos:
-        agregar_archivos_nuevos_a_zip(archivos, "backup.zip")
-        subir_archivo_a_drive("backup.zip")
+        if agregar_archivos_nuevos_a_zip(archivos, "backup.zip"):
+            subir_archivo_a_drive("backup.zip")
+        else:
+            print("No hay archivos nuevos, no se crea ni sube el backup.")
     else:
         print("No hay archivos para comprimir y subir.")
 
