add6a240d4b6d6f500292c546d686f2931071f43
Tibiie <yuraniecheverri8@gmail.com>
2025-02-17 12:58:49 -0500

    git

--- backup.py
+++ backup.py
@@ -21,14 +21,21 @@ def comprimir_archivos(archivos, zip_path):
 def agregar_archivos_nuevos_a_zip(archivos, zip_path):
     """Verifica si hay archivos nuevos y los agrega al ZIP sin descomprimirlo."""
     archivos_drive = obtener_archivos_en_drive(DRIVE_FOLDER_ID)
+    
+    archivos_drive_nombres = {archivo['name']: archivo for archivo in archivos_drive}
+
     archivos_locales = {os.path.basename(file): file for file in archivos}
     
-    archivos_nuevos = [file for file in archivos_locales if file not in archivos_drive]
+    print(f"Archivos locales a comparar: {archivos_locales}")
+    print(f"Archivos en Google Drive: {archivos_drive_nombres}")
+    
+    archivos_nuevos = [
+        file for file in archivos_locales
+        if os.path.basename(file) not in archivos_drive_nombres
+    ]
     
     if archivos_nuevos:
         print(f"Archivos nuevos a agregar: {archivos_nuevos}")
         comprimir_archivos([archivos_locales[file] for file in archivos_nuevos], zip_path)
-        return True 
     else:
         print("No hay archivos nuevos para agregar.")
-        return False  
--- conexion_drive.py
+++ conexion_drive.py
@@ -9,15 +9,19 @@ def get_drive_service():
     creds = service_account.Credentials.from_service_account_info(credentials_dict)
     return build("drive", "v3", credentials=creds)
 
-def obtener_archivos_en_drive(folder_id):
-    """Obtiene los archivos almacenados en Google Drive en una carpeta específica."""
+def obtener_archivos_en_drive(drive_folder_id):
+    """Obtiene los archivos almacenados en una carpeta específica de Google Drive."""
     try:
         service = get_drive_service()
-        results = service.files().list(q=f"'{folder_id}' in parents", pageSize=1000, fields="nextPageToken, files(id, name)").execute()
+        results = service.files().list(
+            q=f"'{drive_folder_id}' in parents",
+            pageSize=10,
+            fields="nextPageToken, files(id, name)"
+        ).execute()
         items = results.get('files', [])
 
         if not items:
-            print("No se encontraron archivos en Drive.")
+            print("No se encontraron archivos en Google Drive.")
         else:
             print("Archivos en Google Drive:")
             for item in items:
