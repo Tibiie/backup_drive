773dc1212a9541eeaf78b41451f8081d064ed3b7
Tibiie <yuraniecheverri8@gmail.com>
2025-02-17 14:48:37 -0500

    f

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from conexion_database import obtener_archivos_desde_bd
 def main():
     archivos = obtener_archivos_desde_bd()
     if archivos:
+        print(f"Archivos a comprimir: {archivos}") 
         if agregar_archivos_nuevos_a_zip(archivos, "backup.zip"):
             print("Archivos nuevos encontrados, subiendo el backup.")
             subir_archivo_a_drive("backup.zip")
